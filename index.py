72a052fc2adaefb866dc44bfe73932bb6f3cc757
Adebowaleola18 <115582174+Adebowaleola18@users.noreply.github.com>
2022-10-11 19:08:42 +0100

    Add files via upload

---
+++ index.py
@@ -0,0 +1,18 @@
+import math
+print("hello")
+a = int(input("insert number a"))
+b = int(input("insert number b"))
+c = int(input("insert number c"))
+
+
+descriminant = (b*b) - (4*a*c)
+
+if descriminant > 0:
+    root1 = ((-b + math.sqrt(descriminant))/(2*a))
+    root2 = ((-b - math.sqrt(descriminant))/(2*a))
+    print("the roots of the equation are " f'{root1}' " and " f'{root2}')
+elif descriminant == 0:
+    root1 = root2 = -b / (2*a)
+    print("the roots of the equations are the same and is " f'{root1}')
+else:
+    print("The equation has no real root")
